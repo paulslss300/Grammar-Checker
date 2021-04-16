8d7f1f4afa120dcc5297fb962e0916fb31e6a6a0
Paul Tang <50609667+paulslss300@users.noreply.github.com>
2021-04-16 13:06:41 -0700

    Update grammar_checking_tree.py

--- grammar_checking_tree.py
+++ grammar_checking_tree.py
@@ -3,10 +3,42 @@ This file contains the GrammarCheckingTree class, which is a recursive tree data
 that represents a constituent parse tree of an English sentence. This class has methods
 for checking grammar rules.
 
+This file also contains the Feedback class, which represents a grammar-checking
+feedback returned by the grammar-checking methods in GrammarCheckingTree.
+
 This file is Copyright (c) 2021 Yuzhi Tang, Hongshou Ge, Zheng Luan.
 """
 from typing import Optional
 from grammar_tree import GrammarTree
+
+
+class Feedback:
+    """
+    This class represents a grammar-checking feedback returned by the grammar-
+    checking methods in GrammarCheckingTree.
+
+    Instance Attributes:
+        - type: integer representing the type of feedback.
+        - type: string description of the type of feedback.
+        - message: message accompanied with the feedback.
+
+    Representation Invariants:
+        - self.type in {"Possible Error", "Test Ineffective", "Error Undetected"}
+    """
+    type: int
+    type_str: str
+    message: str
+
+    def __init__(self, type: int, msg: str = "") -> None:
+        """
+        Precondition:
+            - type in [1, 2, 3]
+        """
+        assert type in [1, 2, 3]
+        type_map = {1: "Error Undetected", 2: "Possible Error", 3: "Test Ineffective"}
+        self.type = type
+        self.type_str = type_map[type]
+        self.message = msg
 
 
 class GrammarCheckingTree(GrammarTree):
@@ -37,11 +69,10 @@ class GrammarCheckingTree(GrammarTree):
                            'r3': self.check_noun_to_verb,
                            'r4': self.check_end_punctuation,
                            'r5': self.existence_of_subject,
-                           'r6': self.multiverbs_match_mistake,
-                           'r7': self.check_complete_sentence,
-                           'r8': self.check_adjective,
-                           'r9': self.check_verb,
-                           'r10': self.check_parallelism
+                           # 'r6': self.check_complete_sentence,
+                           # 'r7': self.check_adjective,
+                           # 'r8': self.check_verb,
+                           # 'r9': self.check_parallelism
                            }
         assert rules_lst == ["*"] or all(r in methods_mapping for r in rules_lst)
 
@@ -52,47 +83,59 @@ class GrammarCheckingTree(GrammarTree):
         else:
             checks_lst = rules_lst
         for rule in checks_lst:
-            feedback.append(methods_mapping[rule]())
+            fb = methods_mapping[rule]()
+            if fb.message == "":
+                feedback.append(f'{rule}: {fb.type_str}.')
+            else:
+                feedback.append(f'{rule}: {fb.type_str}. {fb.message}')
         return feedback
 
     # ----------------------------------------------------------------
     # ------------- Below are Joseph's methods ------------------------
     # ----------------------------------------------------------------
 
-    def plural_noun_singular_verb(self) -> str:
+    def plural_noun_singular_verb(self) -> Feedback:
         """As part of the subject-verb agreement rule, this method checks whether a
         plural noun is mistakenly matched to a singular verb and then return feedback.
 
-        For some hard-to-determine situations, the feedback would not give a definite
-        answer regarding the correctness of the sentence by this rule.
+        E.g. "The ships sail away." is wrong while "The ships sails away." is correct.
+
+        IMPORTANT: This method may be ineffective for certain sentence types (see
+        Discussion in project report).
+
+        Example usages see TODO in main.py.
 
         Precondition:
-            - The sentence does not start with a pronoun.
+            - The sentence does not start with a pronoun as the subject.
         """
         first = self
         while first.subtrees != []:
             first = first.subtrees[0]
         if first.root['label'] == 'PRP':
-            return 'The subject is pronoun. This method can not determine.'
-        result_mistake = 'This sentence may mistakenly match singular verb to plural nouns.'
+            return Feedback(3, 'The sentence starts with a pronoun as the subject.')
+        err_feedback = Feedback(2, 'A plural noun is mistakenly matched to a singular verb.')
         # Exist plural nouns. No and. No singular nouns. Exist third singular verb.
         if self.contain_type('NNS') and not self.contain_type('CC') \
                 and not self.contain_type('NN') and self.contain_type('VBZ'):
-            return result_mistake
+            return err_feedback
         #  Only check sentences and sub-sentence.
         # for subtree in self.get_subtrees():
         for subtree in self.subtrees:
             if subtree.root['label'] == 'S':
-                if subtree.plural_noun_singular_verb() == result_mistake:
-                    return result_mistake
-        return 'Maybe it is correct or it is hard to determine.'
+                if subtree.plural_noun_singular_verb().type == 2:
+                    return err_feedback
+        return Feedback(1)
 
-    def singular_noun_plural_verb(self) -> str:
+    def singular_noun_plural_verb(self) -> Feedback:
         """As part of the subject-verb agreement rule, this method checks whether a
         singular noun is mistakenly matched to a plural verb and then return feedback.
 
-        For some hard-to-determine situations, the feedback would not give a definite
-        answer regarding the correctness of the sentence by this rule.
+        E.g. "The ship sail away." is wrong while "The ship sails away." is correct.
+
+        IMPORTANT: This method may be ineffective for certain sentence types (see
+        Discussion in project report).
+
+        Example usages see TODO in main.py.
 
         Precondition:
             - The sentence does not start with a pronoun.
@@ -101,116 +144,110 @@ class GrammarCheckingTree(GrammarTree):
         while first.subtrees != []:
             first = first.subtrees[0]
         if first.root['label'] == 'PRP':
-            return 'The subject is pronoun. This method can not determine.'
-        result_mistake = 'This sentence may mistakenly match plural verb to singular nouns.'
+            return Feedback(3, 'The sentence starts with a pronoun as the subject.')
+        err_feedback = Feedback(2, 'A singular noun is mistakenly matched to a plural verb.')
         # Exist singular Noun. No and. No plural nouns.
         # Exist verb phrase. Exist third singular verb.
-        #
         if self.contain_type('NN') and not self.contain_type('NNS') and not \
                 self.contain_type('CC') and self.contain_type('VP'):
             if not self.contain_type('VBD') and not self.contain_type('VBZ'):
-                return result_mistake
+                return err_feedback
         # Only check sentence and sub-sentence.
         for s in self.subtrees:
             if s.root['label'] == 'S':
-                if s.singular_noun_plural_verb() == result_mistake:
-                    return result_mistake
-        return 'Maybe it is correct or it is hard to determine.'
+                if s.singular_noun_plural_verb() == 2:
+                    return err_feedback
+        return Feedback(1)
 
-    def check_noun_to_verb(self) -> str:
+    def check_noun_to_verb(self) -> Feedback:
         """As part of the subject-verb agreement rule, this method uses 2 helpers to check
         whether a singular noun is mistakenly matched to a plural verb or whether a plural
         noun is mistakenly matched to a singular verb and then return feedback.
 
-        For some hard-to-determine situations, the feedback would not give a definite
-        answer regarding the correctness of the sentence by these 2 rules.
+        IMPORTANT: This method may be ineffective for certain sentence types (see
+        Discussion in project report).
+
+        Example usages see TODO in main.py.
 
         Precondition:
             - The sentence does not start with a pronoun.
         """
-        result_so_far = set()
-        result_so_far.add(self.singular_noun_plural_verb())
-        result_so_far.add(self.plural_noun_singular_verb())
-        result_mistake1 = 'This sentence may mistakenly match singular verb to plural nouns.'
-        result_mistake2 = 'This sentence may mistakenly match plural verb to singular nouns.'
-        if result_mistake1 in result_so_far and result_mistake2 not in result_so_far:
-            return result_mistake1
-        elif result_mistake2 in result_so_far and result_mistake1 not in result_so_far:
-            return result_mistake2
-        elif result_mistake2 in result_so_far and result_mistake1 in result_so_far:
-            return result_mistake2 + ' ' + result_mistake1
+        feedback1 = self.singular_noun_plural_verb()
+        feedback2 = self.plural_noun_singular_verb()
+        feedback_type = max(feedback1.type, feedback2.type)
+        if feedback1.message == feedback2.message:
+            msg = feedback1.message
+        elif feedback1.message == "":
+            msg = feedback2.message
+        elif feedback2.message == "":
+            msg = feedback1.message
         else:
-            return 'Maybe it is correct or it is hard to determine.'
+            msg = feedback1.message + ", and " + feedback2.message
+        return Feedback(feedback_type, msg)
 
-    def check_end_punctuation(self) -> str:
+    def check_end_punctuation(self) -> Feedback:
         """Check whether the end punctuation of the sentence represented by the tree
-        is correct and return feedback.
+        is correct and return feedback. End punctuation in this case refers to only
+        ".", "!", and "?".
+
+        E.g. "What is he doing." does not have correct end punctuation as it should
+        have been ended with "?". "He is good" also does not have correct end punctuation
+        as it lacks an end punctuation. "He is good." does have correct end punctuation.
+
+        IMPORTANT: This method may be ineffective for certain sentence types (see
+        Discussion in project report).
+
+        Example usages see TODO in main.py.
         """
         if not self.contain_content('!') and not self.contain_content('?') \
                 and not self.contain_content('.'):
-            return 'This sentence is not ended with exclamation mark, period mark or question mark.'
+            return Feedback(2, "Sentence not ended with '.', '!' or '?'.")
         if self.contain_type('SBARQ') or self.contain_type('SQ'):
             if self.find_the_last() == '?':
-                return 'The question sentence ended correctly.'
+                return Feedback(1, "Sentence has a good end punctuation.")
         else:
             if self.find_the_last() == '.' or self.find_the_last() == '!':
-                return 'This sentence has a good end punctuation.'
+                return Feedback(1, "Sentence has a good end punctuation.")
             else:
-                return 'The end of this sentence is very likely to have a wrong punctuation.'
-        return 'Something special happens. Can not detect this sentence.'
+                return Feedback(2, "Sentence has a wrong punctuation.")
+        return Feedback(3, 'Something special happens. Can not detect this sentence.')
 
-    def existence_of_subject(self) -> str:
+    def existence_of_subject(self) -> Feedback:
         """Check whether this sentence has a subject.
+
+        IMPORTANT: This method may be ineffective for certain sentence types (see
+        Discussion in project report).
+
+        Example usages see TODO in main.py.
         """
-        if (self.root['label'] == 'ROOT' or self.root['label'] == 'S') \
-                and not self.contain_type('NP'):
-            return 'There is no subject in the sentence.'
+        if self.root['label'] == 'S' and not self.contain_type('NP'):
+            return Feedback(2, 'There is no subject in the sentence.')
         l_copy = self.subtrees.copy()
         l2 = l_copy.copy()
         for ss in l2:
             if ss.root['label'] == 'VP':
                 l_copy.remove(ss)
         if any(i.contain_type('NP') for i in l_copy) is False:
-            return 'There is likely being no subject in the sentence.'
+            return Feedback(2, 'There is no subject in the sentence.')
         else:
-            return 'Maybe it is correct or it is hard to determine.'
-
-    def multiverbs_match_mistake(self) -> str:
-        """Check whether a simple (combining) sentence contains more than one verb.
-        TODO: edit docstring
-        TODO: edit method name
-        """
-        result_a = 'There may be multiple verbs match to one noun in a simple sentence.'
-        if not self.contain_type('VP'):
-            return 'There is no verb phrase in this sentence.'
-        the_sentence = self
-        vp_lst = [tree for tree in the_sentence.subtrees if tree.root['label'] == 'VP']
-        vb_set = {'VBZ', 'VBP', 'VBD', 'VBN', 'VP'}
-        vb_use_set = {'VBZ', 'VBP', 'VBD'}
-        for vp in vp_lst:
-            l_copy = vp.subtrees.copy()
-            for vbx in vp.subtrees:
-                if vbx.root['label'] in vb_set:
-                    l_copy.remove(vbx)
-                    neo_l_copy = [i for i in l_copy if i.root['label'] in vb_use_set]
-                    if neo_l_copy != []:
-                        return result_a
-
-        return 'No such kind of mistakes have been detected, yet.'
+            return Feedback(1, "There is likely a subject in the sentence.")
 
     # ----------------------------------------------------------------
     # ------------- Below are Caules' methods ------------------------
     # ----------------------------------------------------------------
+# Check whether the sentence represented by the tree has a noun phrase
+#             and a verb phrase (i.e. minimum requirement for the sentence to be
+#             complete).
 
-    def check_complete_sentence(self) -> bool:
-        """Check whether the sentence has noun and verb
-        Precondition:
-            - self.root['label'] == 'ROOT'
+    def check_complete_sentence(self) -> Feedback:
+        """Check whether the sentence represented by the tree has a noun phrase
+        and a verb phrase (i.e. minimum requirement for the sentence to be
+        complete).
         """
         if not (self.contain_type('NP') and self.contain_type('VP')):
-            return False
+            return Feedback(2, "Sentence is incomplete.")
         else:
-            return True
+            return Feedback(1, "Sentence is complete.")
 
     def check_adjective(self, whether_question: Optional[bool] = False) -> Optional[str]:
         """According to the grammar book, the position of adj has 2 cases:
@@ -280,7 +317,7 @@ class GrammarCheckingTree(GrammarTree):
                 # usually, it should be linking-verb + adj
                 condition5 = self.subtrees[0].root['text'] == 'am' or self.subtrees[0].root[
                     'text'] == 'is' or self.subtrees[0].root['text'] == 'are' or self.subtrees[0] \
-                    .root['text'] == 'was' or self.subtrees[0].root['text'] == 'were'
+                                 .root['text'] == 'was' or self.subtrees[0].root['text'] == 'were'
                 if self.subtrees[1].root['label'] == 'JJ' \
                         or self.subtrees[1].root['label'] == 'ADJP' \
                         and condition5:
@@ -338,12 +375,12 @@ class GrammarCheckingTree(GrammarTree):
             elif self.root['label'] == 'VP' or self.root['label'] == 'S':
                 condition1 = self.subtrees[0].root['text'] == 'am' or self. \
                     subtrees[0].root['text'] == 'is' or self.subtrees[0].root['text'] == 'are' \
-                    or self.subtrees[0].root['text'] == 'was'
+                             or self.subtrees[0].root['text'] == 'was'
                 # be/like + verbing
                 if condition1 and self.subtrees[1].subtrees[0].root['label'] == 'VBG':
                     return None
                 if self.subtrees[0].root[
-                        'text'] == 'were' or self.subtrees[0].root['text'] == 'like' \
+                    'text'] == 'were' or self.subtrees[0].root['text'] == 'like' \
                         or self.subtrees[0].root['text'] == 'likes' \
                         and self.subtrees[1].subtrees[0].subtrees[0].root['label'] == 'VBG':
                     return None
